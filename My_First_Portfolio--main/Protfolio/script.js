2928c916d3a85eda74cc193bbb9f09799e5f1624
Dahami Jayawardhane <dahamijayawardhane@gmail.com>
2025-10-07 03:52:43 +0530

    Add files via upload

---
+++ My_First_Portfolio--main/Protfolio/script.js
@@ -0,0 +1,5 @@
+// Optional: If you want to add more dynamic effects later.
+document.getElementById('designer-text').addEventListener('animationend', () => {
+    console.log('UI/UX Designer animation completed!');
+});
+
---
+++ My_First_Portfolio--main/README.md
@@ -0,0 +1,2 @@
+# My_First_Portfolio-
+This is my 1st portfolio website, i created. This is just a start only. 
